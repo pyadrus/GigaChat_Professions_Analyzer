f4a6f15e47385e30388628803cec242421ad256f
PyAdminRU <zh.vitaliy92@gmail.com>
2024-12-16 13:44:54 +0300

    Парсинг и перебор файла Excel.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_gigachat.chat_models import GigaChat
 from loguru import logger
+import openpyxl as op
 
 # Загрузка переменных окружения из файла .env
 load_dotenv(dotenv_path='settings/config.env')
@@ -12,6 +13,22 @@ GIGA_CHAT = os.getenv('GIGA_CHAT')
 logger.info(f'GIGA_CHAT: {GIGA_CHAT}')
 
 
+def open_list_gup():
+    """"Открытие таблицы и чтение данных с таблицы Excel"""
+    wb = op.load_workbook('file.xlsx')
+    ws = wb.active
+    list_gup = []
+
+    for row in ws.iter_rows(min_row=1, max_row=68, min_col=1, max_col=2):
+        row_data = [cell.value for cell in row]
+        list_gup.append(row_data)
+        print(row_data)
+
+    return list_gup
+
+
+
+
 def system_prompt(work):
     """Промпт для ИИ"""
     return f"Расскажите, чем занимается данная профессия на предприятии: {work}"
@@ -19,6 +36,15 @@ def system_prompt(work):
 
 def get_chat_completion_gigachat():
     """Получение ответа от GigaChat."""
+
+    # Получаем данные из таблицы Excel
+    list_gup = open_list_gup()
+    for i in list_gup:
+        plot = i[0]
+        work = i[1]
+        print(f"Участок: {plot}, Профессия: {work}")
+
+
     try:
         # Создание экземпляра модели GigaChat
         llm = GigaChat(
@@ -29,8 +55,7 @@ def get_chat_completion_gigachat():
             streaming=False,
         )
 
-        # Профессия, которую мы хотим рассмотреть
-        work = "Начальник участка"
+        work = "Начальник участка"  # Профессия, которую мы хотим рассмотреть
 
         # Формирование сообщения для отправки в модель
         messages = [
@@ -38,9 +63,7 @@ def get_chat_completion_gigachat():
             HumanMessage(content=""),  # Здесь должно быть сообщение пользователя, но у вас оно пустое
         ]
 
-        # Отправка запроса и получение ответа
-        response = llm.invoke(messages)
-        logger.debug(response.content)
+        response = llm.invoke(messages)  # Отправка запроса и получение ответа
 
         return response.content
 
